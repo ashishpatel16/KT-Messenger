184bfc1c7d100c6851616193efd169035cdd371e
Ashish <ashishpatel16.2000@gmail.com>
2021-06-10 04:43:22 +0530

    Added Navigation Drawer

--- app/src/main/java/com/ashish/messenger/MainActivity.kt
+++ app/src/main/java/com/ashish/messenger/MainActivity.kt
@@ -5,13 +5,18 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.*
 import com.ashish.messenger.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseException
 import com.google.firebase.FirebaseTooManyRequestsException
@@ -25,24 +30,41 @@ class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
     private var user : FirebaseUser? = null
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         user = Firebase.auth.currentUser
-
-        
+        drawerLayout = binding.drawerLayout
         navController = this.findNavController(R.id.nav_host_fragment)
 
 
-
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.loginFragment,R.id.chatsFragment),drawerLayout)
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
 
 
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
+                || super.onOptionsItemSelected(item)
+    }
+
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return navController.navigateUp(appBarConfiguration)
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }else {
+            super.onBackPressed()
+        }
     }
 
     companion object{
